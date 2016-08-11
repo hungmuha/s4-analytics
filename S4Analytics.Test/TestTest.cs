b645d227b695b8e0dc5ed6138720e56166a5c1d6
Nathaniel Wingfield <nw@ufl.edu>
2016-08-11 10:04:44 -0400

    reorganize solution; add tests

---
+++ S4Analytics.Test/TestTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace S4Analytics.Tests
+{
+    public class TestTest
+    {
+        [Fact]
+        public void TwoPlusTwo()
+        {
+            Assert.Equal(2 + 2, 4);
+        }
+    }
+}
