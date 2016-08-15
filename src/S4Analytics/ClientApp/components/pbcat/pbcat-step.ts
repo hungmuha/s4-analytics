804c15ed9edadde832b816355960e2a8391ff6fa
Nathaniel Wingfield <nw@ufl.edu>
2016-08-15 14:47:42 -0400

    add empty pbcat components

---
+++ src/S4Analytics/ClientApp/components/pbcat/pbcat-step.ts
@@ -0,0 +1,9 @@
+﻿import * as ng from '@angular/core';
+
+@ng.Component({
+    selector: 'pbcat-step',
+    template: require('./pbcat-step.html')
+})
+export class PbcatStep {
+
+}
---
+++ src/S4Analytics/ClientApp/components/pbcat/pbcat-summary.ts
@@ -0,0 +1,9 @@
+﻿import * as ng from '@angular/core';
+
+@ng.Component({
+    selector: 'pbcat-summary',
+    template: require('./pbcat-summary.html')
+})
+export class PbcatSummary {
+
+}
