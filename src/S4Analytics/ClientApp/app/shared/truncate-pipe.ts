a737ce6473b8fa4328f56625c0ae8a5d3f8f987e
Nathaniel Wingfield <nw@ufl.edu>
2016-12-01 15:01:24 -0500

    remove logging

--- src/S4Analytics/ClientApp/app/shared/truncate-pipe.ts
+++ src/S4Analytics/ClientApp/app/shared/truncate-pipe.ts
@@ -5,8 +5,6 @@
 })
 export class TruncatePipe {
     transform(value: string, limit: string = '10', trail: string = '...'): string {
-        console.log(limit);
-        console.log(trail);
         let limitNum = parseInt(limit);
         return value.length > limitNum ? value.substring(0, limitNum) + trail : value;
     }
