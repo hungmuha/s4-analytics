0c1ff0a6b28ee0d9a6bf6784229825f6a4b8ab78
marni fowler <mfowler@ufl.edu>
2016-12-27 14:05:08 -0500

    Created a state service for new user request app

---
+++ src/S4Analytics/ClientApp/app/admin/new-user-request/shared/new-user-request.service.ts
@@ -0,0 +1,51 @@
+﻿import { Injectable } from '@angular/core';
+import { NewUserRequest } from './new-user-request';
+
+
+@Injectable()
+export class NewUserRequestService {
+
+    getNewUserRequests(): NewUserRequest[] {
+        let nur1 = new NewUserRequest(
+            123,
+            new Date('12/12/2017'),
+            'request desc',
+            2,
+            0,
+            null,
+            7,
+            1,
+            '',
+            '',
+            'Sara',
+            'Yorty',
+            '',
+            'syorty@alachua.gov',
+            null,null, null, null, null, null, null, null,
+            null, null, null, null, null, null, null, null);
+
+        let nur2 = new NewUserRequest(
+            124,
+            new Date('12/13/2017'),
+            'request desc',
+            4,
+            5,
+            null,
+            6,
+            9,
+            '',
+            '',
+            'Jim',
+            'Jomes',
+            '',
+            'jones@alachua.gov',
+            null, null, null, null, null, null, null, null,
+            null, null, null, null, null, null, null, null);
+
+        return [nur1, nur2];
+    }
+
+
+
+
+}
