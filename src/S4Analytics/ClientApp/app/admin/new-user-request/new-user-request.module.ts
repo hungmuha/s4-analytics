1af7e3a2aff4943c17f6858af81af061647d513d
marni fowler <mfowler@ufl.edu>
2016-12-28 13:33:00 -0500

    Implemented request action window as a modal pop up

---
+++ src/S4Analytics/ClientApp/app/admin/new-user-request/new-user-request.module.ts
@@ -0,0 +1,30 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TruncatePipe } from '../../shared';
+import { adminRoutes } from '../admin.routing';
+import { AdminComponent } from '../admin.component';
+import { RequestQueueComponent } from './request-queue.component';
+import { RequestActionComponent } from './request-action.component';
+import { NewUserRequestStateService } from './shared/new-user-request-state.service';
+import { NewUserRequestService } from './shared/new-user-request.service';
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(adminRoutes),
+        CommonModule,
+        FormsModule,
+        NgbModule.forRoot()
+    ],
+    declarations: [
+        RequestQueueComponent,
+        RequestActionComponent
+    ],
+    providers: [
+        NewUserRequestService,
+        NewUserRequestStateService
+    ]
+})
+export class RequestQueueModule { }
