2c2c54ab78782595e7fd448c0f0a33c889b1e42d
marni fowler <mfowler@ufl.edu>
2016-12-28 13:37:38 -0500

    Removed unused imports

--- src/S4Analytics/ClientApp/app/admin/admin.module.ts
+++ src/S4Analytics/ClientApp/app/admin/admin.module.ts
@@ -6,8 +6,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TruncatePipe } from '../shared';
 import { adminRoutes } from './admin.routing';
 import { AdminComponent } from './admin.component';
-import { RequestQueueComponent } from './new-user-request/request-queue.component';
-import { RequestActionComponent } from './new-user-request/request-action.component';
 import { NewUserRequestStateService } from './new-user-request/shared/new-user-request-state.service';
 import { NewUserRequestService } from './new-user-request/shared/new-user-request.service';
 import { RequestQueueModule } from './new-user-request/new-user-request.module';
