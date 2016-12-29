f474ef2a99ea7955464ae082edea6a3cc83dac5d
marni fowler <mfowler@ufl.edu>
2016-12-29 11:27:10 -0500

    Cleaned up more usused imports

--- src/S4Analytics/ClientApp/app/admin/new-user-request/new-user-request.module.ts
+++ src/S4Analytics/ClientApp/app/admin/new-user-request/new-user-request.module.ts
@@ -1,7 +1,6 @@
 ﻿import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RequestQueueComponent } from './request-queue.component';
 import { RequestActionComponent } from './request-action.component';
 import { NewUserRequestStateService, NewUserRequestService } from '../shared';
