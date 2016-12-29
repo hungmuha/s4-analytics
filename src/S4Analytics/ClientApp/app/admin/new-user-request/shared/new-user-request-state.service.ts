a4d1765a051ff6150a9c9c415d5ff4979e4c0cda
marni fowler <mfowler@ufl.edu>
2016-12-29 10:32:55 -0500

    Created a barrel file for items in shared folder

--- src/S4Analytics/ClientApp/app/admin/new-user-request/shared/new-user-request-state.service.ts
+++
@@ -1,7 +0,0 @@
-﻿import { Injectable } from '@angular/core';
-import { NewUserRequest } from './new-user-request';
-
-@Injectable()
-export class NewUserRequestStateService {
-    newUserRequests: NewUserRequest[];
-}
