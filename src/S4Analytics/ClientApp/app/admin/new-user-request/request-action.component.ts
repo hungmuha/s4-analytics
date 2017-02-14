c62c99162c3303096d4f20b2291b5ac17d5c2234
marni fowler <mfowler@ufl.edu>
2017-02-14 12:23:31 -0500

    Removed stub code to handle email and user creation from client side

--- src/S4Analytics/ClientApp/app/admin/new-user-request/request-action.component.ts
+++ src/S4Analytics/ClientApp/app/admin/new-user-request/request-action.component.ts
@@ -49,136 +49,13 @@ export class RequestActionComponent  {
     }
 
     private processOKResult(): void {
-        switch (this.state.selectedRequest.requestStatus) {
-            case NewUserRequestStatus.NewUser:
-                console.log('create new user, record completed');
 
-                this.processNewUserCreated();
-                break;
-            case NewUserRequestStatus.NewConsultant:
-                console.log('create new consultant, record completed');
-
-                this.processNewConsultantCreated();
-                if (this.sendEmail(this.getNewConsultantCredentialsEmail())) {
-                    this.updateStatus(NewUserRequestStatus.Completed);
-                }
-                break;
-            case NewUserRequestStatus.NewContractor:
-                console.log('create new contractor, update status to NewConsultant, open NewConsultant dialog');
-
-                this.processNewContractorCreated();
-                break;
-            case NewUserRequestStatus.NewAgency:
-                console.log('send email to global admin to create new agency, update status to CreateAgency');
-
-                this.processRequestNewAgency();
-                break;
-            case NewUserRequestStatus.CreateAgency:
-                console.log('verify new agency created, update status to NewUser');
-                this.processNewAgencyCreated();
-                break;
-            default:
-                console.log('default');
-                break;
-        }
     }
 
     private processRejectedResult(): void {
 
-        switch (this.state.selectedRequest.requestStatus) {
-            case NewUserRequestStatus.NewUser:
-                console.log('email requestor why rejected');
-                break;
-            case NewUserRequestStatus.NewConsultant:
-                console.log('email requestor why rejected');
-                break;
-            case NewUserRequestStatus.NewContractor:
-                console.log('email requestor why rejected');
-                break;
-            case NewUserRequestStatus.NewAgency:
-                console.log('email requestor why rejected');
-                break;
-            case NewUserRequestStatus.CreateAgency:
-                console.log('email requestor why rejected');
-                break;
-            default:
-                console.log('default');
-                break;
-        }
-
-        this.updateStatus(NewUserRequestStatus.Rejected);
-
     }
 
-    private processNewUserCreated(): void {
-        this.createNewUser();
-        this.updateStatus(NewUserRequestStatus.Completed);
-        this.sendEmail(this.getNewUserCredentialsEmail());
-    }
 
-    private processNewConsultantCreated(): void {
-        this.createNewConsultant();
-        this.updateStatus(NewUserRequestStatus.Completed);
-        this.sendEmail(this.getNewConsultantCredentialsEmail());
-    }
-
-    private processRequestNewAgency(): void {
-        this.updateStatus(NewUserRequestStatus.CreateAgency);
-        this.sendEmail(this.getCreateAgencyEmail());
-    }
-
-    private processNewAgencyCreated(): void {
-        this.updateStatus(NewUserRequestStatus.NewUser);
-        this.sendEmail(this.getApproveNewUserEmail());
-    }
-    private processNewContractorCreated(): void {
-        this.updateStatus(NewUserRequestStatus.NewConsultant);
-        // open New Consultant dialog
-    }
-
-    private sendEmail(emailContent: string): void {
-        console.log(emailContent);
-
-    }
-
-    private getNewUserCredentialsEmail(): string {
-
-        return '';
-    }
-
-    private getNewConsultantCredentialsEmail(): string {
-
-        return '';
-    }
-
-    private getCreateAgencyEmail(): string {
-
-        return '';
-    }
-
-    private getApproveNewUserEmail(): string {
-
-        return '';
-    }
-
-    private createNewUser(): void {
-        // create a new user in database
-
-        this.updateCreatedDt(Date.now());
-    }
-
-    private createNewConsultant(): void {
-        // create a new consultant in database
-
-        this.updateCreatedDt(Date.now());
-    }
-
-    private updateCreatedDt(date: number) {
-        this.state.selectedRequest.userCreatedDt = new Date(date);
-    }
-
-    private updateStatus(status: NewUserRequestStatus) {
-        this.state.selectedRequest.requestStatus = status;
-    }
 
 }
