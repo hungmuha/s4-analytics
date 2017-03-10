204f7f8799d66219ea42f0aabf2edf22830410c4
Nathaniel Wingfield <nw@ufl.edu>
2017-03-10 10:48:08 -0500

    upgrade to visual studio 2017

--- src/S4Analytics/webpack.config.vendor.js
+++ src/S4Analytics/webpack.config.vendor.js
@@ -1,80 +1,63 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var extractCSS = new ExtractTextPlugin('vendor.css');
-var merge = require('extendify')({ isDeep: true, arrays: 'concat' });
-var isDevelopment =
-    process.env.ASPNETCORE_ENVIRONMENT === 'Local' ||
-    process.env.ASPNETCORE_ENVIRONMENT === 'Development';
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const merge = require('webpack-merge');
 
-if (process.env.ASPNETCORE_ENVIRONMENT) {
-    console.log('ASP.NET Core ' + process.env.ASPNETCORE_ENVIRONMENT + ' environment detected.');
+module.exports = (env) => {
+    const extractCSS = new ExtractTextPlugin('vendor.css');
+    const isDevBuild = !(env && env.prod);
+
+    const config = {
+        stats: { modules: false },
+        resolve: { extensions: ['.js'] },
+        module: {
+            rules: [
+                { test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, use: 'url-loader?limit=100000' },
+                { test: /\.css(\?|$)/, use: extractCSS.extract({ use: 'css-loader' }) }
+            ]
+        },
+        entry: {
+            vendor: [
+                '@angular/common',
+                '@angular/compiler',
+                '@angular/core',
+                '@angular/forms',
+                '@angular/http',
+                '@angular/platform-browser',
+                '@angular/platform-browser-dynamic',
+                '@angular/router',
+                '@ng-bootstrap/ng-bootstrap',
+                'angular2-universal',
+                'angular2-universal-polyfills',
+                'bootstrap/dist/css/bootstrap.css',
+                'es6-shim',
+                'es6-promise',
+                'event-source-polyfill',
+                'font-awesome/css/font-awesome.css',
+                'lodash',
+                'moment',
+                'rxjs',
+                'zone.js'
+            ]
+        },
+        output: {
+            publicPath: '/dist/',
+            filename: '[name].js',
+            library: '[name]_[hash]',
+            path: path.join(__dirname, 'wwwroot', 'dist')
+        },
+        plugins: [
+            new webpack.ContextReplacementPlugin(/\@angular\b.*\b(bundles|linker)/, path.join(__dirname, './ClientApp')), // Workaround for https://github.com/angular/angular/issues/11580
+            new webpack.IgnorePlugin(/^vertx$/), // Workaround for https://github.com/stefanpenner/es6-promise/issues/100
+            extractCSS,
+            new webpack.DllPlugin({
+                path: path.join(__dirname, 'wwwroot', 'dist', '[name]-manifest.json'),
+                name: '[name]_[hash]'
+            })
+        ].concat(isDevBuild ? [] : [
+            new webpack.optimize.UglifyJsPlugin()
+        ])
+    };
+
+    return config;
 }
-else {
-    console.log('No ASP.NET Core environment detected.');
-}
-
-var commonConfig = {
-    resolve: {
-        extensions: [ '.js' ]
-    },
-    module: {
-        loaders: [
-            { test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/, loader: 'url-loader?limit=100000' },
-            { test: /\.css/, loader: extractCSS.extract(['css-loader']) }
-        ]
-    },
-    entry: {
-        vendor: [
-            '@angular/common',
-            '@angular/compiler',
-            '@angular/core',
-            '@angular/forms',
-            '@angular/http',
-            '@angular/platform-browser',
-            '@angular/platform-browser-dynamic',
-            '@angular/router',
-            '@ng-bootstrap/ng-bootstrap',
-            'bootstrap/dist/css/bootstrap.css',
-            'core-js',
-            'font-awesome/css/font-awesome.css',
-            'lodash',
-            'moment',
-            'reflect-metadata',
-            'rxjs',
-            'zone.js'
-        ]
-    },
-    output: {
-        path: path.join(__dirname, 'wwwroot', 'dist'),
-        filename: '[name].js',
-        library: '[name]_[hash]',
-    },
-    plugins: [
-        // see https://github.com/angular/angular/issues/11580
-        new webpack.ContextReplacementPlugin(
-          /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
-          './src'
-        ),
-        extractCSS,
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.DllPlugin({
-            path: path.join(__dirname, 'wwwroot', 'dist', '[name]-manifest.json'),
-            name: '[name]_[hash]'
-        })
-    ]
-};
-
-var devConfig = { };
-
-var prodConfig = {
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false },
-            minimize: true,
-            mangle: false // Due to https://github.com/angular/angular/issues/6678
-        })
-    ]
-};
-
-module.exports = merge(commonConfig, isDevelopment ? devConfig : prodConfig);
