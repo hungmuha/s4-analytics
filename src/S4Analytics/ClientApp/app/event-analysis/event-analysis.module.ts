6d70373f89936dcf46c1cb53d28406f773f20c0e
Nathaniel Wingfield <nw@ufl.edu>
2017-05-02 11:14:29 -0400

    ngx-datatable and ag-grid poc

--- src/S4Analytics/ClientApp/app/event-analysis/event-analysis.module.ts
+++ src/S4Analytics/ClientApp/app/event-analysis/event-analysis.module.ts
@@ -2,21 +2,32 @@
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { GridModule } from '@progress/kendo-angular-grid';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { AgGridModule } from 'ag-grid-angular/main';
 import { CrashService } from './shared';
 import { EventAnalysisComponent } from './event-analysis.component';
 import { EventMapComponent } from './event-map.component';
 import { KendoPocComponent } from './kendo-poc.component';
+import { NgxDatatablePocComponent } from './ngx-datatable-poc.component';
+import { AgGridPocComponent, RedComponent } from './ag-grid-poc.component';
 
 @NgModule({
     imports: [
         RouterModule,
         CommonModule,
-        GridModule
+        GridModule,
+        NgxDatatableModule,
+        AgGridModule.withComponents(
+            [RedComponent]
+        )
     ],
     declarations: [
         EventAnalysisComponent,
         EventMapComponent,
-        KendoPocComponent
+        KendoPocComponent,
+        NgxDatatablePocComponent,
+        AgGridPocComponent,
+        RedComponent
     ],
     providers: [
         CrashService
