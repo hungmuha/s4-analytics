04e7e0603990f5edf8349d31b0796e7129235eaa
Nathaniel Wingfield <nw@ufl.edu>
2017-05-03 09:06:06 -0400

    lint fixes

--- src/S4Analytics/ClientApp/app/event-analysis/ag-grid-poc.component.ts
+++ src/S4Analytics/ClientApp/app/event-analysis/ag-grid-poc.component.ts
@@ -4,7 +4,7 @@ import { GridOptions } from 'ag-grid';
 
 @Component({
     selector: 'red-component',
-    template: `<span style="color: red">{{ params.value }}</span>`
+    template: `<span style='color: red'>{{ params.value }}</span>`
 })
 export class RedComponent {
     private params: any;
@@ -23,13 +23,13 @@ export class AgGridPocComponent implements OnInit {
         this.gridOptions = {};
         this.gridOptions.columnDefs = [
             {
-                headerName: "ID",
-                field: "id",
+                headerName: 'ID',
+                field: 'id',
                 width: 100
             },
             {
-                headerName: "Value",
-                field: "value",
+                headerName: 'Value',
+                field: 'value',
                 cellRendererFramework: RedComponent,
                 width: 100
             },
@@ -39,7 +39,7 @@ export class AgGridPocComponent implements OnInit {
             { id: 5, value: 10 },
             { id: 10, value: 15 },
             { id: 15, value: 20 }
-        ]
+        ];
     }
     ngOnInit() { }
 }
--- src/S4Analytics/ClientApp/app/event-analysis/ngx-datatable-poc.component.ts
+++ src/S4Analytics/ClientApp/app/event-analysis/ngx-datatable-poc.component.ts
@@ -2,13 +2,13 @@
 // import { CrashService, CrashQuery } from './shared';
 
 export class Page {
-    //The number of elements in the page
+    // The number of elements in the page
     size: number = 0;
-    //The total number of elements
+    // The total number of elements
     totalElements: number = 0;
-    //The total number of pages
+    // The total number of pages
     totalPages: number = 0;
-    //The current page number
+    // The current page number
     pageNumber: number = 0;
 }
 
