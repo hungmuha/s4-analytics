64af95c81a8364d129f040bd25f28beeb6a62ef4
marni fowler <mfowler@ufl.edu>
2017-05-15 12:32:17 -0400

    Removed unused contract-viewer.component

--- src/S4Analytics/ClientApp/app/admin/new-user-request/new-user-request.routing.ts
+++
@@ -1,7 +0,0 @@
-﻿import { Routes } from '@angular/router';
-import { ContractViewerComponent } from './contract-viewer.component';
-
-
-export const newUserRequestRoutes: Routes = [
-    { path: 'contract-viewer/:requestNumber', component: ContractViewerComponent },
-];
