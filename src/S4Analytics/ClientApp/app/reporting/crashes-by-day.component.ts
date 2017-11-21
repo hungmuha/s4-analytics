b3c9b09565d64339966f240d64d2660ee2a8b190
Nathaniel Wingfield <nw@ufl.edu>
2017-11-21 11:39:37 -0500

    refactor

--- src/S4Analytics/ClientApp/app/reporting/crashes-by-day.component.ts
+++ src/S4Analytics/ClientApp/app/reporting/crashes-by-day.component.ts
@@ -1,9 +1,13 @@
 ﻿import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
-import * as Highcharts from 'highcharts';
 import * as Highstock from 'highcharts/highstock';
 import * as moment from 'moment';
-import { ReportingService, CrashesOverTimeQuery, ReportOverTime } from './shared';
+import {
+    ReportingService,
+    CrashesOverTimeQuery,
+    ReportOverTime,
+    CrashesByDayFormatter
+} from './shared';
 
 @Component({
     selector: 'crashes-by-day',
@@ -75,9 +79,7 @@ export class CrashesByDayComponent implements OnInit, OnChanges {
         return this.maxDate !== undefined ? this.maxDate.format('MMMM DD, YYYY') : '';
     }
 
-    constructor(private reporting: ReportingService) {
-        let formatter = new Formatter();
-    }
+    constructor(private reporting: ReportingService) { }
 
     ngOnInit() {
         this.alignByWeek = true;
@@ -150,8 +152,8 @@ export class CrashesByDayComponent implements OnInit, OnChanges {
             },
             tooltip: {
                 formatter: this.alignByWeek
-                    ? (new Formatter()).formatAlignedByWeek // do not use arrow function because highcharts provides the context for the format method
-                    : (new Formatter()).formatAlignedByDate
+                    ? (new CrashesByDayFormatter()).formatAlignedByWeek // do not use arrow function because highcharts provides the context for the format method
+                    : (new CrashesByDayFormatter()).formatAlignedByDate
             }
         };
         this.chart.update(options);
@@ -188,53 +190,5 @@ export class CrashesByDayComponent implements OnInit, OnChanges {
         }
         let startUtc = Date.UTC(startDate.year(), startDate.month(), startDate.date());
         return startUtc;
-    }
-}
-
-class Formatter {
-    /*  * Available data are:
-        * this.percentage (not shared) / this.points[i].percentage (shared)
-        *   Stacked series and pies only. The point's percentage of the total.
-        * this.point (not shared) / this.points[i].point (shared)
-        *   The point object. The point name, if defined, is available through this.point.name.
-        * this.points
-        *   In a shared tooltip, this is an array containing all other properties for each point.
-        * this.series (not shared) / this.points[i].series (shared)
-        *   The series object. The series name is available through this.series.name.
-        * this.total (not shared) / this.points[i].total (shared)
-        *   Stacked series only. The total value at this point's x value.
-        * this.x
-        *   The x value. This property is the same regardless of the tooltip being shared or not.
-        * this.y (not shared) / this.points[i].y (shared)
-        *   The y value.
-    */
-    points: any[];
-    x: number;
-    y: number;
-
-    formatAlignedByWeek(): string {
-        let dt = new Date(this.x);
-        let mt1 = moment(dt);
-        let mt2 = mt1.clone().subtract(364, 'days');
-        let mts = this.points.length > 1 ? [mt2, mt1] : [mt1];
-        let s = `<b>${mt1.toDate()}</b><br />`;
-        for (let i = 0; i < this.points.length; i++) {
-            let point = this.points[i];
-            s += `<span style="color:${point.color}">\u25CF</span> ${point.series.name}: <b>${mts[i].toDate()}, ${point.y}</b><br/>`;
-        }
-        return s;
-    }
-
-    formatAlignedByDate(): string {
-        let dt = new Date(this.x);
-        let mt1 = moment(dt);
-        let mt2 = moment([mt1.year() - 1, mt1.month(), mt1.date()]);
-        let mts = this.points.length > 1 ? [mt2, mt1] : [mt1];
-        let s = `<b>${mt1.toDate()}</b><br />`;
-        for (let i = 0; i < this.points.length; i++) {
-            let point = this.points[i];
-            s += `<span style="color:${point.color}">\u25CF</span> ${point.series.name}: <b>${mts[i].toDate()}, ${point.y}</b><br/>`;
-        }
-        return s;
     }
 }
--- src/S4Analytics/ClientApp/app/reporting/shared/index.ts
+++ src/S4Analytics/ClientApp/app/reporting/shared/index.ts
@@ -1,3 +1,4 @@
 ﻿export { ReportOverTime } from './report-over-time';
 export { ReportingService } from './reporting.service';
 export { CrashesOverTimeQuery } from './crashes-over-time-query';
+export { CrashesByDayFormatter } from './crashes-by-day-formatter';
