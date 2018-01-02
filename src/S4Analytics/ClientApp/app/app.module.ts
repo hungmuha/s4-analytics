d8c02158aa1caa46a2751f697ffdf4b1e7981de4
Nathaniel Wingfield <nw@ufl.edu>
2018-01-02 17:19:18 -0500

    add kendo grid

--- src/S4Analytics/ClientApp/app/app.module.ts
+++ src/S4Analytics/ClientApp/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GridModule } from '@progress/kendo-angular-grid';
 import { routes } from './app.routing';
 import { AdminModule } from './admin';
 import { EventAnalysisModule } from './event-analysis';
@@ -23,6 +24,7 @@ import { PROVIDERS } from './shared';
         BrowserModule,
         FormsModule,
         NgbModule.forRoot(),
+        GridModule,
         AdminModule,
         EventAnalysisModule,
         NetworkAnalysisModule,
