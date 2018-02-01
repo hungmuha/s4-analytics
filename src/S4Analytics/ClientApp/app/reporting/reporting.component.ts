262e8dd9f166ef3cedd5b8e078df4c30d7f84b30
Nathaniel Wingfield <nw@ufl.edu>
2018-02-01 17:29:53 -0500

    add submenu to expose citation reports

--- src/S4Analytics/ClientApp/app/reporting/reporting.component.ts
+++ src/S4Analytics/ClientApp/app/reporting/reporting.component.ts
@@ -1,6 +1,20 @@
 ﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './reporting.component.html'
 })
-export class ReportingComponent { }
+export class ReportingComponent {
+    constructor(private router: Router) { }
+
+    get currentMode(): string {
+        switch (this.router.url) {
+            case '/reporting/crashes-over-time':
+                return 'Crashes';
+            case '/reporting/citations-over-time':
+                return 'Citations';
+            default:
+                return '';
+        }
+    }
+}
